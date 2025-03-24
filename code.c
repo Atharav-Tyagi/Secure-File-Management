64a93e9b680a2c516d3a51b00ca252c90bc76f1d
ANIL0023 <anilkumar9999q@gmail.com>
2025-03-24 19:40:28 +0530

    Updated activity.log function

--- code.c
+++ code.c
@@ -20,6 +20,13 @@ typedef struct {
 User users[MAX_USERS] = { {"admin", "admin123"} };
 int user_count = 1;
 
+void log_activity(const char *username, const char *action, const char *filename) {
+    FILE *log_file = fopen("activity.log", "a");
+    if (!log_file) return;
+    time_t now = time(NULL);
+    fprintf(log_file, "%s - User: %s, Action: %s, File: %s\n", ctime(&now), username, action, filename);
+    fclose(log_file);
+}
 
 
 void menu(const char *username) {
